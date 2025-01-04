b6627d71d2816bb2677985c9ff57804e6b9598f1
MuhammadRukon <muhammad.rukon242@gmail.com>
2025-01-04 15:16:14 +0600

    feat: bomb creation with game over functionality

--- src/game/scenes/create.ts
+++ src/game/scenes/create.ts
@@ -6,7 +6,13 @@ let platforms: Phaser.Physics.Arcade.StaticGroup;
 let score: number = 0;
 let scoreText: Phaser.GameObjects.Text;
 
-let stars: Phaser.Physics.Arcade.Group;
+type GroupType = Phaser.Physics.Arcade.Group;
+
+let stars: GroupType;
+let bomb: Sprite;
+let bombs: GroupType;
+let gameOver: boolean = false;
+
 export default function create(this: Phaser.Scene) {
     // background
     this.add.image(400, 300, "sky");
@@ -66,12 +72,41 @@ export default function create(this: Phaser.Scene) {
     this.physics.add.overlap(player, stars, collectStar, null, this);
     function collectStar(player: Sprite, star: Sprite) {
         star.disableBody(true, true);
-        // update score
+
         score += 10;
         scoreText.setText("Score: " + score);
+
+        if (stars.countActive(true) === 0) {
+            stars.children.iterate(function (child) {
+                child.enableBody(true, child.x, 0, true, true);
+            });
+
+            var x =
+                player.x < 400
+                    ? Phaser.Math.Between(400, 800)
+                    : Phaser.Math.Between(0, 400);
+
+            bomb = bombs.create(x, 16, "bomb");
+            bomb.setBounce(1);
+            bomb.setCollideWorldBounds(true);
+            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        }
+    }
+    bombs = this.physics.add.group();
+
+    this.physics.add.collider(bombs, platforms);
+
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
+
+    function hitBomb(player, bomb) {
+        this.physics.pause();
+
+        player.setTint(0xff0000);
+        player.anims.play("turn");
+
+        gameOver = true;
     }
 
-    //display score
     scoreText = this.add.text(16, 16, "score: 0", {
         fontSize: "32px",
         fill: "#000",
